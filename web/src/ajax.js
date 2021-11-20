d60ccb20fe62015002c52b25eb62a0e1a2338e8c
sanlangguo <SanLangGuo@outlook.com>
2021-11-20 23:53:33 +0800

    feat: http 监听

--- web/src/ajax.js
+++ web/src/ajax.js
@@ -1,34 +1,35 @@
+/* eslint-disable no-undef */
 export function ajax(options) {
-  options = options || {}
-  options.type = (options.type || 'GET').toUpperCase()
-  options.dataType = options.dataType || 'json'
-  var params = formatParams(options.data)
-
+  options = options || {};
+  options.type = (options.type || 'GET').toUpperCase();
+  options.dataType = options.dataType || 'json';
+  var params = formatParams(options.data);
+  var xhr = null;
   if (window.XMLHttpRequest) {
-    var xhr = new XMLHttpRequest()
+    xhr = new XMLHttpRequest();
   } else {
-    var xhr = new ActiveXObject('Microsoft.XMLHTTP')
+    xhr = new ActiveXObject('Microsoft.XMLHTTP');
   }
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      var status = xhr.status
+      var status = xhr.status;
       if (status >= 200 && status < 300) {
-        options.success && options.success(xhr.responseText, xhr.responseXML)
+        options.success && options.success(xhr.responseText, xhr.responseXML);
       } else {
-        options.fail && options.fail(status)
+        options.fail && options.fail(status);
       }
     }
-  }
+  };
 
   if (options.type == 'GET') {
-    xhr.open('GET', options.url + '?' + params, true)
-    xhr.send(null)
+    xhr.open('GET', options.httpUrl + '?' + params, true);
+    xhr.send(null);
   } else if (options.type == 'POST') {
-    xhr.open('POST', options.url, true)
+    xhr.open('POST', options.httpUrl, true);
     // 设置表单提交时的内容类型
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.send(params)
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
   }
 }
 
@@ -37,9 +38,9 @@ export function ajax(options) {
   *格式化参数
   */
 function formatParams(data) {
-  var arr = []
+  var arr = [];
   for (var name in data) {
-    arr.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]))
+    arr.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
   }
-  return arr.join('&')
+  return arr.join('&');
 }
